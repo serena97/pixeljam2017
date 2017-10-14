f4673e660c810d3c628730b2f4306754e4967784
Serena Chan <lok.chan@kcl.ac.uk>
2017-10-14 22:41:48 +0100

    applied fox refab

--- Assets/Scripts/DialogueHolder.cs
+++ Assets/Scripts/DialogueHolder.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DialogueHolder : MonoBehaviour {
 
 	public string[] dialogue;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -16,7 +17,10 @@ public class DialogueHolder : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D collider) {
-		DialogueManager.instance.Show (dialogue);
+		GameManager gameManager = GameManager.GetInstance();
+		if (collider.gameObject == gameManager.player.gameObject) {
+			DialogueManager.instance.Show (dialogue);
+		}
 	}
 	void OnTriggerExit2D(Collider2D collider) {
 		DialogueManager.instance.Hide ();
