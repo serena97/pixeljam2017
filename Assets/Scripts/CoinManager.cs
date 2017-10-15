49e87b6133a2771129b30ce8cd67e182a45faaed
Serena Chan <lok.chan@kcl.ac.uk>
2017-10-15 01:47:57 +0100

    fixed errors

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinManager : MonoBehaviour {
 
-	public Text coinText;
+	private Text coinText;
 	public int coinCounter; 
 
 	// Use this for initialization
 	void Start () {
-		coinText = coinCounter;
+		coinText = GetComponent<Text> ();
+		coinText.text = coinCounter.ToString();
 	}
 	
 	// Update is called once per frame
