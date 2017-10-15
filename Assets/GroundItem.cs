848046f69bbf1d20a6b8000bbd2c2f90501160cd
Serena Chan <lok.chan@kcl.ac.uk>
2017-10-15 05:32:52 +0100

    Changes.

---
+++ Assets/GroundItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundItem : MonoBehaviour {
+
+	public Item item;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if(collider.gameObject == Player.instance.gameObject) {
+			if(InventoryDisplay.instance.PickupItem(item))
+				Destroy(gameObject);
+		}
+	}
+}
